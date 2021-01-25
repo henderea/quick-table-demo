7211758e3c27bf941b6e29a509580965374af753
Eric Henderson <henderea@gmail.com>
2021-01-25 15:30:28 -0500

    add sorting and define types for each column to assist sorting and filtering

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import './index.scss';
 import * as $ from 'jquery';
 import * as _ from 'lodash';
-import { JQueryQuickTable, QuickTable, Column, setup } from '@henderea/quick-table';
+import { JQueryQuickTable, QuickTable, Column, SortDef, setup } from '@henderea/quick-table';
 
 setup($, _);
 
@@ -26,46 +26,76 @@ if(process.env.NODE_ENV === 'production') {
 declare interface Entry {
   fn: string;
   ln: string;
+  favNum: number;
 }
 
 const data: Entry[] = [
   {
     fn: 'Eric',
-    ln: 'Henderson'
+    ln: 'Henderson',
+    favNum: 8
   },
   {
     fn: 'Michael',
-    ln: 'Henderson'
+    ln: 'Henderson',
+    favNum: 7
   },
   {
     fn: 'John',
-    ln: 'Goins'
+    ln: 'Goins',
+    favNum: 3
   },
   {
     fn: 'Ken',
-    ln: 'Grafals'
+    ln: 'Grafals',
+    favNum: 11
   }
 ];
 
 $(function() {
+  const displaySortOrders = (table: QuickTable<Entry>) => {
+    const sortOrders: SortDef[] = table.sortOrders;
+    table.columns.each((c: Column<Entry>) => {
+      const orderIndex: number = _.findIndex(sortOrders, (o: SortDef) => o[0] == c.index);
+      const span: JQuery = c.$head.find('.sort-order');
+      if(orderIndex >= 0) {
+        span.text(`${orderIndex + 1}: ${sortOrders[orderIndex][1]}`);
+      } else {
+        span.text('-');
+      }
+    });
+  };
   let qTable: QuickTable<Entry> = $('#quickTable').QuickTable((table: QuickTable<Entry>) => {
     table.columnDefs = [
       {
         data: 'fn',
+        type: 'string',
         cssClass: 'center'
       },
-      { data: 'ln' },
-      { render(data: any, row: Entry) { return `${row.fn} ${row.ln}`; } },
+      {
+        data: 'ln',
+        type: 'string'
+      },
+      {
+        type: 'string',
+        render(data: any, row: Entry) { return `${row.fn} ${row.ln}`; }
+      },
       {
         data: 'fn',
+        type: 'html',
         html: true,
         render(data: string, row: Entry) { return `<b>${row.ln}</b>, ${data}`; }
+      },
+      {
+        data: 'favNum',
+        type: 'num'
       }
     ];
+    table.sortOrders = [[0, 'asc'], [1, 'asc']];
     table.columns.each((c: Column<Entry>) => {
       const $check: JQuery = $(`input[data-column="${c.index}"]`);
       const col: Column<Entry> = c;
-      c.on('column.visible', e => { $check.prop('checked', e.visible); })
+      c.on('column.visible', (e: any) => { $check.prop('checked', e.visible); })
        .trigger('column.visible', { visible: c.visible });
       $check.on('change', () => { col.visible = $check.is(':checked'); });
       let searchField: HTMLInputElement = c.$head.find('input').get(0);
@@ -74,6 +104,10 @@ $(function() {
         const filter: string = ($this.val() || '') as string;
         col.setFilter(filter, true)
            .applyFilters();
+      });
+      col.$head.eq(0).on('click', () => {
+        table.toggleSort(col.index);
+        displaySortOrders(table);
       });
     });
     table.data = data;
@@ -89,6 +123,7 @@ $(function() {
            );
     });
   }).draw() as QuickTable<Entry>;
+  displaySortOrders(qTable);
   qTable.clearFilters();
   $('#export-link').on('click', () => {
     let headers: string[] = ['First', 'Last', 'First Last', 'Last, First'];
